9dedbda3fefec1376b0abed3fd07472a7e6bf789
VigilBushido <vigilbushido@gmail.com>
2025-07-16 19:02:50 -0400

    +fixed - current Date

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,10 @@
 function formatDate(date: Date): string {
     const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric'};
-    //let formatedDate = new Date(date).toLocaleDateString(undefined, options);
+    //let formattedDate = new Date(date).toLocaleDateString(undefined, options);
     //let newDate = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
     //
     const formattedDate = new Date(date).setDate(date.getDate() + 1);
-    const currentDate = new Date(formattedDate).toLocaleDateString(undefined, options)
-    return currentDate;
+    return new Date(formattedDate).toLocaleDateString(undefined, options);
 }
 
 export {formatDate};
