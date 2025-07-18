84c3166582071a5f45751ad04b9c5e1306d264e7
VigilBushido <vigilbushido@gmail.com>
2025-07-18 02:38:22 -0400

    +capitalize util

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+// Formate date correctly to a string
 function formatDate(date: Date): string {
     const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric'};
     //let formattedDate = new Date(date).toLocaleDateString(undefined, options);
@@ -7,4 +8,12 @@ function formatDate(date: Date): string {
     return new Date(formattedDate).toLocaleDateString(undefined, options);
 }
 
-export {formatDate};
+// Capitalize the first letter
+function capitalize(str: string): string {
+    if (str.length === 0){
+        return str;
+    }
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export {formatDate, capitalize};
