30457dca73ddc01019f2b76db77c7526989ff6cd
VigilBushido <vigilbushido@gmail.com>
2025-07-23 22:04:24 -0400

    api endpoint GET completed

--- src/pages/api/search.json.ts
+++ src/pages/api/search.json.ts
@@ -1,6 +1,8 @@
 import type { APIRoute } from "astro";
+import { getCollection } from 'astro:content';
+import type { CollectionEntry } from 'astro:content';
 
-export const GET: APIRoute = ({ url }): Response => {
+export const GET: APIRoute = async ({ url }): Promise<Response> => {
     const query: string | null = url.searchParams.get('query');
 
     //Handle if a query is not present
@@ -15,7 +17,19 @@ export const GET: APIRoute = ({ url }): Response => {
         });
     }
 
-    return new Response(JSON.stringify({ query }), {
+    const allBlogArticles: CollectionEntry<'blog'>[] = await getCollection('blog');
+
+    const searchResults = allBlogArticles.filter(article => {
+        const titleMatch: boolean = article.data.title.toLowerCase().includes(query!.toLowerCase())
+
+        const bodyMatch: boolean = article.body.toLowerCase().includes(query!.toLowerCase());
+
+        const slugMatch: boolean = article.slug.toLowerCase().includes(query!.toLowerCase());
+
+        return titleMatch || bodyMatch || slugMatch;
+    });
+
+    return new Response(JSON.stringify({ searchResults }), {
         status: 200,
         headers: {
             'Content-Type': 'application/json'
